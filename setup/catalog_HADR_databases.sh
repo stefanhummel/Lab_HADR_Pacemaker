62bed2d8690b5ea237c1512e60b75613e4587536
stefanhummel <stefan.hummel@gmail.com>
2021-04-16 22:04:35 +0200

    Update catalog_HADR_databases.sh

--- setup/catalog_HADR_databases.sh
+++ setup/catalog_HADR_databases.sh
@@ -3,13 +3,13 @@
 see /home/db2inst1/ALSM_app/setup.sh
 
 # catalog database on server3
-db2 catalog TCPIP NODE srv1 REMOTE server1 SERVER 50000
+db2 catalog TCPIP NODE srv1 REMOTE 10.0.0.101 SERVER 50000
 db2 CATALOG DATABASE sample AS db_srv1 AT NODE srv1
-db2 update alternate server for database DB_SRV1  using hostname server2  port 50000
+db2 update alternate server for database DB_SRV1  using hostname 10.0.0.101  port 50000
 
 # uncatalog
 db2 UNCATALOG DATABASE db_srv1
-# db2 uncatalog TCPIP NODE srv1
+# db2 uncatalog  NODE srv1
 
 db2 connect to db_srv1 user db2inst1 using db2inst1
 # ...
