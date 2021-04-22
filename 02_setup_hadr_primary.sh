834acf2745e62b9a92ac53149749796e11795d28
Andreas Christian <achristian@de.ibm.com>
2021-04-22 10:57:56 +0200

    Update 02_setup_hadr_primary.sh

--- 02_setup_hadr_primary.sh
+++ 02_setup_hadr_primary.sh
@@ -3,9 +3,6 @@
 # Enable log archiving
 db2 update db cfg for sample using LOGARCHMETH1 "DISK:/home/db2inst1/sample_arch1/"
 
-# Take an offline backup
-# db2 backup database sample to /home/db2inst1/sample_backup
-
 # Setting up HADR cfg parameters on Primary database
 db2 update db cfg for sample using HADR_LOCAL_HOST server1       # server name of HADR primary 
 db2 update db cfg for sample using HADR_LOCAL_SVC 5005           # port of HADR primary
