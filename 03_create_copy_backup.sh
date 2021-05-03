ecf6482d9177539e4266ac0145f337d859d7645e
stefanhummel <stefan.hummel@gmail.com>
2021-05-03 08:49:39 +0200

    Update 03_create_copy_backup.sh

--- 03_create_copy_backup.sh
+++ 03_create_copy_backup.sh
@@ -8,7 +8,7 @@ db2 backup database sample to /home/db2inst1/sample_backup
 
 # Copy the backup to server2
 cd /home/db2inst1/sample_backup
-ssh db2inst1@server2 ' mkdir /home/db2inst1/training_backup'
-ssh db2inst1@server2 ' rm /home/db2inst1/training_backup/*'
+ssh db2inst1@server2 ' mkdir /home/db2inst1/sample_backup'
+ssh db2inst1@server2 ' rm /home/db2inst1/sample_backup/*'
 scp -p `ls -Art /home/db2inst1/sample_backup | tail -n 1` db2inst1@server2:/home/db2inst1/sample_backup
 
