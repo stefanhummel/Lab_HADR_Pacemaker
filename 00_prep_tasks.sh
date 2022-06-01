a3e73def774322943252a45fc0f49a73c991c8bc
stefanhummel <stefan.hummel@gmail.com>
2022-06-01 10:24:12 +0200

    update Db2 FP7

--- 00_prep_tasks.sh
+++ 00_prep_tasks.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 
-
 LICFILE=./db2de.lic
 if [ -f "$LICFILE" ]; then
     echo "Add $LICFILE to Db2 licenses."
     db2licm -a $LICFILE
+    db2stop
+    db2start
 fi
 
-db2stop
-db2start
 
