b9f3c21c95b39ad283460b6091fa2bd8cf3afbeb
stefanhummel <stefan.hummel@gmail.com>
2022-06-29 16:39:06 +0200

    Update 03_create_copy_backup.sh
    fixes

--- 03_create_copy_backup.sh
+++ 03_create_copy_backup.sh
@@ -8,6 +8,7 @@ db2 -v backup database sample to /home/db2inst1/sample_backup
 
 # Copy the backup to server2
 cd /home/db2inst1/sample_backup
+ssh db2inst1@server2 'if [ ! -d /home/db2inst1/sample_backup ];then mkdir /home/db2inst1/sample_backup;fi'
 ssh db2inst1@server2 ' mkdir /home/db2inst1/sample_backup'
 ssh db2inst1@server2 ' rm /home/db2inst1/sample_backup/*'
 scp -p `ls -Art /home/db2inst1/sample_backup | tail -n 1` db2inst1@server2:/home/db2inst1/sample_backup
