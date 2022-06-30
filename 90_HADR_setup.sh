b7b8552282e87b6961c56e78f56d0966204aadb0
stefanhummel <stefan.hummel@gmail.com>
2022-06-30 14:57:20 +0200

    Update 90_HADR_setup.sh

--- 90_HADR_setup.sh
+++ 90_HADR_setup.sh
@@ -7,8 +7,8 @@
 ssh db2inst1@server1 ' rm -rf /home/db2inst1/sample_backup/* '
 ssh db2inst1@server1 ' rm -rf /home/db2inst1/sample_arch1 '
 
-ssh db2inst1@server1 ' mkdir /home/db2inst1/sample_backup '
-ssh db2inst1@server1 ' mkdir /home/db2inst1/sample_arch1 '
+ssh db2inst1@server1 'if [ ! -d /home/db2inst1/sample_backup ];then mkdir /home/db2inst1/sample_backup;fi' 
+ssh db2inst1@server1 'if [ ! -d /home/db2inst1/sample_arch1 ];then mkdir /home/db2inst1/sample_arch1;fi' 
 
 # restart database on server1
 ssh db2inst1@server1 ' db2stop force '
@@ -44,4 +44,28 @@ ssh db2inst1@server1 ' db2 -v backup database sample to /home/db2inst1/sample_ba
 ssh db2inst1@server2 'if [ ! -d /home/db2inst1/sample_backup ];then mkdir /home/db2inst1/sample_backup;fi' 
 ssh db2inst1@server2 ' rm -rf /home/db2inst1/sample_backup/* '
 ssh db2inst1@server1 ' scp -p `find /home/db2inst1/sample_backup | tail -n 1` db2inst1@server2:/home/db2inst1/sample_backup '
+
+# restore database on server2
+ssh db2inst1@server2 ' db2start '
+ssh db2inst1@server2 ' db2 -v restore database sample from /home/db2inst1/sample_backup '
+
+# Setting up HADR cfg parameters on standby database
+ssh db2inst1@server2 ' db2 -v update db cfg for sample using HADR_LOCAL_HOST server2  '             #<IP ADDRESS ON STANDBY>
+ssh db2inst1@server2 ' db2 -v update db cfg for sample using HADR_LOCAL_SVC 5005  '                 # ON STANDBY>
+ssh db2inst1@server2 ' db2 -v update db cfg for sample using HADR_REMOTE_HOST server1  '    #<IP ADDRESS ON PRIM>
+ssh db2inst1@server2 ' db2 -v update db cfg for sample using HADR_REMOTE_SVC 5005   '               # ON PRIM>
+ssh db2inst1@server2 ' db2 -v update db cfg for sample using HADR_REMOTE_INST db2inst1  '   # <INSTNAME ON PRIM>
+# Prerequisites for db2cm
+ssh db2inst1@server2 ' db2 -v update db cfg for sample using HADR_SYNCMODE NEARSYNC  '      # or SYNC
+ssh db2inst1@server2 ' db2 -v update db cfg for sample using HADR_PEER_WINDOW 120 '
+
+# starting up HADR on the standby server
+ssh db2inst1@server2 ' db2 -v start hadr on database sample as standby '
+
+# starting up HADR on the primary server
+ssh db2inst1@server1 ' db2 -v start hadr on database sample as primary '
+
+# Verifing HADR is up and running
+# db2pd -db sample -hadr -repeat 1 120
+ssh db2inst1@server1 ' db2pd -db sample -hadr  '
 
